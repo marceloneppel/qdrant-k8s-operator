9ffd4c878d61843e641aa869d9667404ee646b0e
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2025-04-12 13:27:58 -0300

    Start Qdrant
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@gmail.com>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -7,3 +7,19 @@ Build the charm in this git repository using:
 ```shell
 charmcraft pack
 ```
+
+## Deploy the charm
+
+```shell
+# On AMD64.
+juju deploy ./qdrant-k8s_ubuntu@24.04-amd64.charm.charm --resource qdrant-image=qdrant/qdrant:v1.13.6
+
+# On ARM64.
+juju deploy ./qdrant-k8s_ubuntu@24.04-arm64.charm.charm --resource qdrant-image=qdrant/qdrant:v1.13.6
+```
+
+## Test the charm
+
+```shell
+curl http://<qdrant-unit-ip>:6333/collections
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use curl::easy::Easy;
 use rusty_charm_framework::model::ActionModel;
 use rusty_charm_framework::types::ActionResult;
 use rusty_charm_framework::{
@@ -9,8 +10,6 @@ use rusty_charm_framework::{
 };
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::thread::sleep;
-use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all(deserialize = "kebab-case"))]
@@ -23,14 +22,57 @@ fn action_handler(_model: ActionModel<Action, impl Backend>) -> Result<ActionRes
 }
 
 fn event_handler(model: EventModel<impl Backend>) -> Result<Status> {
+    let pattern = "qdrant".to_string();
     match model.event {
-        Event::Install => {
-            model.status.maintenance("installing Qdrant")?;
-            sleep(Duration::from_secs(10));
+        Event::PebbleReady(pattern) => {
+            // Bootstrap the Pebble client.
+            let mut easy = Easy::new();
+            match easy.unix_socket("/charm/containers/qdrant/pebble.socket") {
+                Ok(_) => {
+                    model.log.info("Socket connected")?;
+                }
+                Err(e) => {
+                    model.log.error(format!("Socket error: {}", e).as_str())?;
+                    return Ok(Status::Blocked("Socket error"));
+                }
+            };
+
+            // Add the layer.
+            let mut data = r##"{"action": "add","combine": true,"inner": true,"label": "qdrant","format": "yaml","layer": "summary: Qdrant layer\ndescription: Layer to execute Qdrant.\nservices:\n  qdrant:\n    override: replace\n    summary: Qdrant\n    command: /qdrant/entrypoint.sh\n    startup: enabled"}"##;
+            easy.url("localhost/v1/layers")?;
+            easy.post_field_size(data.len() as u64)?;
+            easy.post(true)?;
+            execute_post_calls(&mut easy, data)?;
+
+            // Start the service.
+            data = r##"{"action": "start","services": ["qdrant"]}"##;
+            easy.url("localhost/v1/services")?;
+            easy.post_field_size(data.len() as u64)?;
+            easy.post(true)?;
+            execute_post_calls(&mut easy, data)?;
         }
         _ => {}
     }
     Ok(Status::Active(""))
+}
+
+fn execute_post_calls(easy: &mut Easy, mut data: &str) -> Result<()> {
+    let mut dst = Vec::new();
+    {
+        let mut transfer = easy.transfer();
+        transfer.read_function(|buf| {
+            let len = std::cmp::min(buf.len(), data.len());
+            buf[..len].copy_from_slice((&data[..len]).as_ref());
+            data = &data[len..];
+            Ok(len)
+        })?;
+        transfer.write_function(|buf| {
+            dst.extend_from_slice(buf);
+            Ok(buf.len())
+        })?;
+        transfer.perform()?;
+    }
+    Ok(())
 }
 
 fn main() -> Result<()> {
